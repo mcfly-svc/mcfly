c2a2c207ef4bfdbe64771c6b87a14971162bddff
Mike Calvanese <mfcalvanese@gmail.com>
2016-05-06 18:35:15 -0400

    Add previously uncommited database migration scripts

---
+++ db/migrations/0001_init_db.down.sql
@@ -0,0 +1,10 @@
+DROP INDEX IF EXISTS project_service_username_name;
+DROP INDEX IF EXISTS provideraccesstoken_user;
+
+DROP TABLE IF EXISTS build;
+DROP TABLE IF EXISTS user_project;
+DROP TABLE IF EXISTS project;
+DROP TABLE IF EXISTS provider_access_token;
+DROP TABLE IF EXISTS marsupi_user;
+
+DROP TYPE IF EXISTS provider;
---
+++ db/migrations/0001_init_db.up.sql
@@ -0,0 +1,34 @@
+CREATE TYPE provider AS ENUM ('github', 'dropbox', 'travis', 'jenkins');
+
+CREATE TABLE marsupi_user (
+	id											serial PRIMARY KEY,
+	name										text,
+	access_token						text
+);
+
+CREATE TABLE provider_access_token (
+	provider								provider,
+	provider_username 			text,
+	access_token 						text,
+ 	user_id									integer REFERENCES marsupi_user(id)
+);
+CREATE UNIQUE INDEX provideraccesstoken_user ON provider_access_token (provider, user_id);
+
+CREATE TABLE project (
+	id											serial PRIMARY KEY,
+	source_provider					provider,
+	handle									text
+);
+
+CREATE TABLE build (
+	id											serial PRIMARY KEY,
+	project_id							integer REFERENCES project(id),
+	hash										text,
+	build_provider					provider
+);
+
+CREATE TABLE user_project (
+	user_id									integer REFERENCES marsupi_user(id),
+	project_id							integer REFERENCES project(id),
+	PRIMARY KEY (user_id, project_id)
+);
